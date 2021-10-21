2fb0bbd7c6883e809f9ff26a07e6c0eb69536e98
Zlatan Stadler <zlat0179@edu.easj.dk>
2021-10-21 19:04:14 +0200

    Added add to card and remove from cart

--- Main.js
+++ Main.js
@@ -1,10 +1,29 @@
 const app = Vue.createApp({
     data: function() {
         return {
-            product: 'logo',
+            cart:0,
+            product: 'SpejlFarver: under solen',
             image: './Assets/images/Circle.jpg',
-            inStock: true,
-
+            inStock: false,
+            details: ['Tadow', 'Balladen', 'Begginers luck', 'Kabob jam', 'Lasses flamingo', 'Time to pretend', 'Graceland', 'first aid kit', 'lasses Ambiland'],
+            variants: [
+                {id: 1, var: 'circle', image: './assets/images/Circle.jpg'},
+                {id: 2, var: 'nocircle', image: './assets/images/NoCircle.jpg'},
+            ],
+            
+        }
+    },
+    methods: {
+        addToCart() {
+            this.cart += 1,
+            this.inventory -= 1
+        },
+        updateImage(variantImage){
+            this.image = variantImage
+        },
+        removeFromCart(){
+            this.cart -= 1,
+            this.inventory += 1
         }
     }
 }).mount('#app')
